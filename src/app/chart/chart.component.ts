0764a2c6cc751963e9b7dbb48b9ba50f1f018e36
Anders Sjöberg <shoobah@gmail.com>
2017-05-13 03:15:45 +0200

    Some randomness

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,3 +1,4 @@
+import { transition } from '@angular/core/src/animation/dsl';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import * as c3 from 'c3';
 
@@ -8,19 +9,18 @@ import * as c3 from 'c3';
   encapsulation: ViewEncapsulation.None
 })
 export class ChartComponent implements OnInit {
-
-  constructor() { }
+  num = 0;
 
   getData(set) {
     let ret = [];
     switch (set) {
-      case 1: ret = [
+      case 0: ret = [
         ['data1', 0, 200, 140, 80, 180, 320, 46],
         ['data2', 0, 10, 30, 90, 180, 360, 720],
         ['data3', 0, 50, 180, 500, 180, 50, 0],
       ];
         break;
-      case 2: ret = [
+      case 1: ret = [
         ['data1', 0, 230, 400, 480, 180, 20, 146],
         ['data2', 0, 430, 300, 9, 380, 360, 70],
         ['data3', 0, 250, 18, 520, 10, 500, 220],
@@ -39,31 +39,28 @@ export class ChartComponent implements OnInit {
       ];
         break;
     }
-
-
     return ret;
   }
 
   displayChart() {
     const chart = c3.generate({
       bindto: '#myChart',
+      transition: {
+        duration: 300
+      },
       data: {
         columns: this.getData(1),
         type: 'spline'
       }
     });
 
-    setTimeout(() => {
+    setInterval(() => {
+      this.num = Math.floor(Math.random() * 4);
       chart.load({
-        columns: this.getData(2)
+        columns: this.getData(this.num)
       });
     }, 2000);
 
-    setTimeout(() => {
-      chart.load({
-        columns: this.getData(3)
-      });
-    }, 4000);
   }
 
   ngOnInit() {
